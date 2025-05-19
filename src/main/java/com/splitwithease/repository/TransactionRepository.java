bbaf2e0790cfc0f2ee6be29485699012477fe7ff
Akshay <akshayjallewar@gmail.com>
2025-05-19 15:30:23 +0530

    Removed UserId from TransactionRepository

--- src/main/java/com/splitwithease/repository/TransactionRepository.java
+++ src/main/java/com/splitwithease/repository/TransactionRepository.java
@@ -1,12 +1,10 @@
 package com.splitwithease.repository;
 
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.splitwithease.model.TransactionEntity;
 
 public interface TransactionRepository extends JpaRepository<TransactionEntity, Long> {
-	List<TransactionEntity> findByUserIdOrderByTimestampDesc(Long userId);
 	}
 
